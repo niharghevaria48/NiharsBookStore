3796241c4c2a5ec54af91bba4ec51b3777c2782d
Nihar Ghevaria <W0806797@myscc.ca>
2023-11-03 20:33:23 -0400

    Created Category Repo

---
+++ NiharsBookstore/NiharsBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using NiharsBooks.Models;
+using NiharsBookstore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NiharsBooks.DataAccess.Repository.IRepository
+{
+    class CategoryRepository :Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+
+        void ICategoryRepository.Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NiharsBookstore/NiharsBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using NiharsBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NiharsBooks.DataAccess.Repository.IRepository
+{
+    interface ICategoryRepository
+    {
+        void Update(Category category);
+    }
+}
