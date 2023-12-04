d4c6d4e9bebf1e16a3bdf0457fadb9c98d8bed3c
niharghevaria <niharghevaria48@gmail.com>
2023-12-04 18:05:58 -0500

    Update in Migration file and checked on other computer

--- NiharsBookstore/NiharsBooks.Utilites/SD.cs
+++ NiharsBookstore/NiharsBooks.Utilites/SD.cs
@@ -7,11 +7,7 @@ namespace NiharsBooks.Utilites
 {
     public static class SD
     {
-        public const string Proc_CoverType_Create = "usp_CreateCoverType";
-        public const string Proc_CoverType_Get = "usp_GetCoverType";
-        public const string Proc_CoverType_GetAll = "usp_GetCoverTypes";
-        public const string Proc_CoverType_Update = "usp_UpdateCoverType";
-        public const string Proc_CoverType_Delete = "usp_DeleteCoverType";
+        
 
     }
 }
